c82a85c7beb629ff013de9d2797a354edb8ebd92
kjohnson422 <73547881+kjohnson422@users.noreply.github.com>
2021-09-13 22:51:05 -0400

    Assignment 2 commit and push

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+#Takes user input for a name and displays on screen
+
 name = input("Enter your name: ")
 print("Hi", name)
+
+#Takes user input for a number and displays on screen that number squared
 
 number = int(input("Enter a number: "))
 number = number * number
 print(number)
+
+#Takes user input for a word and displays the letter count on screen
 
 letterCount = input("Enter a word: ")
 print(len(letterCount))
