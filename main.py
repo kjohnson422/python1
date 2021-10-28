95cd6f742bcc21ff8aca844d38f72fc60b95bdb4
kjohnson422 <73547881+kjohnson422@users.noreply.github.com>
2021-10-28 11:48:41 -0400

    Assignment 6 string1 branch commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import os
+#prompt user to enter something to be reversed
 
-cmd = 'date'
-os.system(cmd)
+print("Enter a line of text:")
+text = input()
+text = text[::-1]
+print(text)
+
+#prompt user to enter a line of text to be converted to Bianry
+print("Enter a line of text:")
+binText = input()
+convert = ''.join(format(ord(i), '08b') for i in binText)
+print("The converted string is now: " + convert)
+
