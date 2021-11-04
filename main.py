78599c4f34f69a7d72893bee154995d083a30757
kjohnson422 <73547881+kjohnson422@users.noreply.github.com>
2021-11-04 14:14:27 -0400

    Assignment 7 regex branch commit

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-# take in user input
-
-def splitString():
-    sentence = input("Print a sentence: ")
-    print(*sentence.split(), sep='\n')
-
-
-#function 2
-def splitString1():
-    userInput = input("Print a sentence: ")
-    for i in userInput.strip().split('\n'):
-        [print(j) for j in i.split()]
-
+import re
+import os
 
 if __name__ == '__main__':
-    splitString()
-    splitString1()
+    if os.access("assignment.txt", os.F_OK):
+        print("The file exists.")
+
+    openTXT = open("assignment.txt", "r")
+    getlines = openTXT.readlines()
+
+    for line in getlines:
+        find = re.findall(".$", line)
+        for i in find:
+            if i == "%":
+                print(f"Line: {line}")
+                print(f"The last character is {i} in the sentence above.")
+
+    if os.access("assignment.txt", os.F_OK):
+        print("The file access.txt exists.")
+    try:
+        os.renames("assignment.txt", "assignment1.txt")
+        print("Changed assignment.txt to assignment1.txt")
+        if os.access("assignment.txt", os.F_OK):
+            print("The file assignment.txt no longer exists.")
+    except:
+        pass
+    if "Users" in os.getcwd():
+        print("Username is: ", re.split("\\\\", os.getcwd())[2])
+    print("Drive is: ", re.split('\\\\', os.getcwd())[0])
+
