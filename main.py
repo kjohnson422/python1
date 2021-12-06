f9df0bfcfbb09c474b5f4d870d402586a29d0289
kjohnson422 <73547881+kjohnson422@users.noreply.github.com>
2021-12-06 16:36:21 -0500

    Assignment 3

--- main.py
+++ main.py
@@ -1,30 +1,69 @@
-import re
-import os
+#Takes user input for a name and displays on screen
+import random
 
-if __name__ == '__main__':
-    if os.access("assignment.txt", os.F_OK):
-        print("The file exists.")
+name = input("Enter your name: ")
+print("Hi", name)
+def printArray():
+    array = [['.', '.', '.', '.', '.', '.'],
+             ['.', 'O', 'O', '.', '.', '.'],
+             ['O', 'O', 'O', 'O', '.', '.'],
+             ['O', 'O', 'O', 'O', 'O', '.'],
+             ['.', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', '.'],
+             ['O', 'O', 'O', 'O', '.', '.'],
+             ['.', 'O', 'O', '.', '.', '.'],
+             ['.', '.', '.', '.', '.', '.']]
 
-    openTXT = open("assignment.txt", "r")
-    getlines = openTXT.readlines()
+#Takes user input for a number and displays on screen that number squared
+    newString = ''
 
-    for line in getlines:
-        find = re.findall(".$", line)
-        for i in find:
-            if i == "%":
-                print(f"Line: {line}")
-                print(f"The last character is {i} in the sentence above.")
+    number = int(input("Enter a number: "))
+    number = number * number
+    print(number)
+    for i in range(len(array)):
+        newString += str(array[i][0])
 
-    if os.access("assignment.txt", os.F_OK):
-        print("The file access.txt exists.")
-    try:
-        os.renames("assignment.txt", "assignment1.txt")
-        print("Changed assignment.txt to assignment1.txt")
-        if os.access("assignment.txt", os.F_OK):
-            print("The file assignment.txt no longer exists.")
-    except:
-        pass
-    if "Users" in os.getcwd():
-        print("Username is: ", re.split("\\\\", os.getcwd())[2])
-    print("Drive is: ", re.split('\\\\', os.getcwd())[0])
+    newString1 = '\n'
+    for i in range(len(array)):
+        newString1 += str(array[i][1])
 
+    newString2 = '\n'
+    for i in range(len(array)):
+        newString2 += str(array[i][2])
+
+    newString3 = '\n'
+    for i in range(len(array)):
+        newString3 += str(array[i][3])
+
+    newString4 = '\n'
+    for i in range(len(array)):
+        newString4 += str(array[i][4])
+
+    newString5 = '\n'
+    for i in range(len(array)):
+        newString5 += str(array[i][5])
+
+    print(newString+newString1+newString2+newString3+newString4+newString5)
+
+#coin flip function
+
+def coinFlip():
+
+    number = int(input("Enter a value 0 or 1"))
+    x = random.randint(0, 1)
+    if x == number:
+        print("You Won!")
+    else:
+        print("You lost")
+
+
+choice = input("Choose a function you would like to run, print an array of . and O or a coin flip")
+if choice == "print array":
+    printArray()
+else:
+    coinFlip()
+
+#Takes user input for a word and displays the letter count on screen
+
+letterCount = input("Enter a word: ")
+print(len(letterCount))
